565b538b93a33e983217d27d1e0400d19b313489
Ana3Lu <anaquiva@unisabana.edu.co>
2026-05-18 08:36:27 -0500

    Actualiza policy eliminando override por compatibilidad

--- groups/Ana/policy.py
+++ groups/Ana/policy.py
@@ -1,15 +1,12 @@
 import numpy as np
-from typing import override
 from connect4.policy import Policy
 
 
 class AnaPolicy(Policy):
 
-    @override
     def mount(self) -> None:
         pass
 
-    @override
     def act(self, s: np.ndarray) -> int:
         rng = np.random.default_rng()
         return int(rng.choice([c for c in range(7) if s[0, c] == 0]))
